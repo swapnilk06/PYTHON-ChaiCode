e49ea7c3261e79d6afc9b6c826b3ee7d3f782a18
Swapnil Kathale <127191602+swapnilk06@users.noreply.github.com>
2024-09-18 16:47:05 +0530

    Add files via upload
    Stand out some questions about automatic creating of  __pycache__ folder how create?

---
+++ 1 - Core python & install python/01_basics/chai.py
@@ -0,0 +1,9 @@
+
+# method call from this chai.py
+
+from hello_chai import chai
+
+chai("ginger tea")
+
+# Q. - Why automatic create "__pycache__" folder & whats work of "__pycache__" folder?
+
---
+++ 1 - Core python & install python/01_basics/hello_chai.py
@@ -0,0 +1,9 @@
+print("chai aur python")
+
+def chai(n):
+      print(n)
+
+chai("lemon tea")
+
+# Q. - Why automatic create "__pycache__" folder & whats work of "__pycache__" folder?
+
